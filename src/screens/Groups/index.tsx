7eb48672b3d22fe3e6917b66fcb2d9b1b0a0a397
fernando <fernandonetodeveloper@gmail.com>
2023-03-17 17:44:22 -0300

    groupcardcomponent

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,18 @@
-import { Header } from "@components/Header"
-import { Highlight } from "@components/Highlight"
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard" ;
 
-import { Container } from "./styles"
+import { Container } from "./styles";
 
 export function Groups() {
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <GroupCard title="rpzdada do ignite" />
     </Container>
   )
 }
