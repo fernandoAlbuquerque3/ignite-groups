96139e2498e4a07e7c5c9a792f2807500756b178
fernando <fernandonetodeveloper@gmail.com>
2023-03-18 16:33:13 -0300

    listempty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from "react-native"
 import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
 import { GroupCard } from "@components/GroupCard"
+import { ListEmpty } from "@components/ListEmpty"
 
 import { Container } from "./styles"
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Fernando's club", "Erikles 'bar "])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -20,6 +21,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && {flex:1}}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não existe nenhuma turma cadastra." />
+        )}
       />
     </Container>
   )
