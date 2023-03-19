4da3c54992b1c2061f460831d7c86f3987fb4ad0
fernando <fernandonetodeveloper@gmail.com>
2023-03-19 19:56:31 -0300

    inputcomponent

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@components/Button"
 import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
+import { Input } from "@components/Input"
+
 import { Container, Content, Icon } from "./styles"
 
 export function NewGroup() {
@@ -16,7 +18,11 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar as pessoas."
         />
 
-        <Button title="Criar" />
+        <Input />
+
+        <Button title="Criar"
+        style={{marginTop: 20}}
+        />
       </Content>
     </Container>
   )
