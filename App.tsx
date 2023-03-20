92911af927facae40dfe72876f90d661930d9215
fernando <fernandonetodeveloper@gmail.com>
2023-03-20 17:29:57 -0300

    players

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import { Groups } from "@screens/Groups"
 
 import  {Loading}  from "./src/components/Loading";
 import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Groups/Players";
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
