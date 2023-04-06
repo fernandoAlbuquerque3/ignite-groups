359d6cf9cbbeb9528f40545926a095e333784069
fernando <fernandonetodeveloper@gmail.com>
2023-04-06 14:39:27 -0300

    routeparams

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Button } from "@components/Button"
 import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
@@ -7,13 +8,15 @@ import { Container, Content, Icon } from "./styles"
 import { useNavigation } from "@react-navigation/native"
 
 export function NewGroup() {
+  const [group, setGroup] = useState("")
 
   const navigation = useNavigation()
-  
-  function handleNew() {
-    navigation.navigate('players', {group: 'salve'})
-  }
 
+  function handleNew() {
+    navigation.navigate("players", { group })
+    //navigation.navigate("players", { group: group })
+  }
+  
   return (
     <Container>
       <Header showBackButton />
@@ -26,7 +29,7 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar as pessoas."
         />
 
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
